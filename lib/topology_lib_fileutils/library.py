48f607ad4e9f45e6482e4d90aeed997f55269c41
Juan Gonzalez <juan-carlos.gonzalez-chacon@hpe.com>
2016-04-05 11:11:14 -0700

    Added documentation

--- lib/topology_lib_fileutils/library.py
+++ lib/topology_lib_fileutils/library.py
@@ -26,11 +26,26 @@ import pexpect
 import requests
 
 def _get_filename(file_path):
+    """
+    Get file name from a given path
+
+    :param file_path: Local or remote location of a given file.
+    :type file_path: string.
+
+    Access to a remote location is allowed only with http[s] protocol.
+    """
     pattern_name = re.compile('(((?<=/)|^)[^/]+$)')
     file_name = pattern_name.findall(file_path)[0][0]
     return file_name
 
 def _get_content_file(file_path):
+    """
+    Get file from the given location and return its content.
+
+    :param str file_path: Local or remote location of a given file.
+    All file location which do not use 'http[s]' in its path,
+    will be handled as local location.
+    """
     remote_path = re.compile('http[s]?://')
     if remote_path.match(file_path):
         command = 'wget {}'.format(file_path)
@@ -39,7 +54,15 @@ def _get_content_file(file_path):
         file_content = open(file_path).read()
     return file_content
 
-def load_file(enode, file_path, shell=None):
+def load_file(enode, file_path):
+    """
+    Load a given file to the remote host(enode)
+    
+    :param enode: Engine node to communicate with.
+    :type enode: topology.platforms.base.BaseNode
+    :param str file_path: Local or remote location of a given file.
+     Remote location is allowed only with http[s] protocol.
+    """
     file_content = _get_content_file(file_path)
     file_name = _get_filename(file_path)
     command = 'echo "{}" >> {}'.format(file_content, file_name)
