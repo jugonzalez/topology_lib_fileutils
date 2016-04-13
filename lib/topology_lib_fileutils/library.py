806ad584b2ce68290173f3a3b4bf350f00ea38de
Juan Gonzalez <juan-carlos.gonzalez-chacon@hpe.com>
2016-04-13 10:18:19 -0700

    change

--- lib/topology_lib_fileutils/library.py
+++ lib/topology_lib_fileutils/library.py
@@ -78,7 +78,8 @@ def load_file(enode, file_name, src_file_path, dst_file_path=None, shell=None):
     command = 'echo "{}" >> {}/{}'.format(file_content, dst_file_path, file_name)
     response = enode(command)
     assert 'No such file or directory' not in response
-    
+   
+    """ 
     cmds = ["python",
             "file = open('{}/{}')".format(dst_file_path, file_name),
             "file_content = codecs.decode(file.read(), 'hex').decode()",
@@ -91,6 +92,7 @@ def load_file(enode, file_name, src_file_path, dst_file_path=None, shell=None):
     for cmd in cmds:
         enode.get_shell("bash").send_command(cmd, matches=">>> ")
     enode.get_shell("bash").send_command("exit()")
+    """
 
 __all__ = [
     'load_file']
